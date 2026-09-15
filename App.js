bd895a05336aeb143a1146c600a4148639facaf0
vini-campos <viniciuscampos131209@gmail.com>
2026-09-15 20:54:44 -0300

    .

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import TabNavigator from './components/TabNavigator';
+import Kanto from './components/Kanto';
+import HomeMap from './components/HomeMap';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    
+      <Drawer.Navigator>
+      
+        <Drawer.Screen name="Home" component={HomeMap} />
+        <Drawer.Screen name="Kanto" component={Kanto} />
+        <Drawer.Screen name="Hoen" component={TabNavigator} />
+
+      </Drawer.Navigator>
+    </NavigationContainer>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
